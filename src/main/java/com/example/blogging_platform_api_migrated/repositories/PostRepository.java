c239c5daf693eacc9f6593ee28d277d164412b8b
raafiAbdul <rafrafyunos@gmail.com>
2026-08-23 12:04:00 +0800

    added a countQuery attribute to the PostRepository findByTerm method

--- src/main/java/com/example/blogging_platform_api_migrated/repositories/PostRepository.java
+++ src/main/java/com/example/blogging_platform_api_migrated/repositories/PostRepository.java
@@ -15,7 +15,12 @@ public interface PostRepository extends CrudRepository<Post, Integer> {
             "where lower(p.title) like lower(concat('%', :term, '%')) " +
             "or lower(p.content) like lower(concat('%', :term, '%')) " +
             "or lower(p.category) like lower(concat('%', :term, '%')) " +
-            "or lower(t) like lower(concat('%', :term, '%'))")
+            "or lower(t) like lower(concat('%', :term, '%'))",
+            countQuery = "select count(p) from Post p left join fetch p.tags t " +
+                    "where lower(p.title) like lower(concat('%', :term, '%')) " +
+                    "or lower(p.content like lower(concat('%', :term, '%')) " +
+                    "or lower(p.category) like lower(concat('%', :term, '%')) " +
+                    "or lower(t) like lower(concat('%', :term, '%'))")
     List<Post> findByTerm(@Param("term") String term);
 
     @Query(value = "select p from Post p where p in :collection")
