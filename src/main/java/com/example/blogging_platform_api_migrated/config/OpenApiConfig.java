34212fdb03e0ff39669f59290e5a4e2728ec7f12
raafiAbdul <rafrafyunos@gmail.com>
2026-09-11 15:44:43 +0800

    fixed server mismatch in OpenApiDefinition annotation

--- src/main/java/com/example/blogging_platform_api_migrated/config/OpenApiConfig.java
+++ src/main/java/com/example/blogging_platform_api_migrated/config/OpenApiConfig.java
@@ -17,7 +17,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         ),
         servers = @Server(
                 description = "Local ENV",
-                url = "https://localhost:8080"
+                url = "http://localhost:8080"
         )
 )
 public class OpenApiConfig {
